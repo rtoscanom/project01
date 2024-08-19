6f3029e17848789c0fdff0e5ffb080d0e465aed8
Raúl Toscano Miranda <rtoscano@correo.unicordoba.edu.co>
2024-08-19 12:32:01 -0500

    changing app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>This app is a simple</div>
+
 </template>
 
 <script>
